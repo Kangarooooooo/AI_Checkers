67388df356e82da44c3b9a61d872653ac7006d8a
Repsack <kh170391@gmail.com>
2016-03-16 13:21:17 +0100

    made functions for placing pieces on the board

--- Project1/Project1/Board.cs
+++ Project1/Project1/Board.cs
@@ -13,5 +13,59 @@ namespace Project1
 {
     class Board
     {
+        int[,] b; //2D array 'b' stores an int that represents 
+        public Board()
+        {
+            b = new int[8, 8]; //8x8 spaces representing the 64 field gameboard.
+        }
+
+        public Boolean redPawnSet(int x, int y) //places red pawn on the field (x,y) returns true if it succeeds
+        {
+            Boolean truth = false; //assumes that functioncall fails
+            if (b[x, y] == 0) //checks if field is empty, represented by 0
+            {
+                truth = true; //success
+                b[x, y] = 1; //place red pawn, represented by -1;
+            }
+            //should position (x,y) not be empty, no overwriting is made 
+            return truth; //returns boolean representing success of this functioncall
+        }
+
+        public Boolean redKingSet(int x, int y) //places red king on the field (x,y) returns true if it succeeds
+        {
+            Boolean truth = false; //assumes that functioncall fails
+            if (b[x, y] == 0) //checks if field is empty, represented by 0
+            {
+                truth = true; //success
+                b[x, y] = 2; //place red king, represented by -1;
+            }
+            //should position (x,y) not be empty, no overwriting is made 
+            return truth; //returns boolean representing success of this functioncall
+        }
+
+        public Boolean blackPawnSet(int x, int y) //places black pawn on the field (x,y) returns true if it succeeds
+        {
+            Boolean truth = false; //assumes that functioncall fails
+            if(b[x,y] == 0) //checks if field is empty, represented by 0
+            {
+                truth = true; //success
+                b[x, y] = -1; //place black pawn, represented by -1;
+            }
+            //should position (x,y) not be empty, no overwriting is made 
+            return truth; //returns boolean representing success of this functioncall
+        }
+
+        public Boolean blackKingSet(int x, int y) //places black king on the field (x,y) returns true if it succeeds
+        {
+            Boolean truth = false; //assumes that functioncall fails
+            if (b[x, y] == 0) //checks if field is empty, represented by 0
+            {
+                truth = true; //success
+                b[x, y] = -2; //place king pawn, represented by -1;
+            }
+            //should position (x,y) not be empty, no overwriting is made 
+            return truth; //returns boolean representing success of this functioncall
+        }
+
     }
 }
