5880bc9c7988afc406db94bd55f44ad86985344d
Repsack <kh170391@gmail.com>
2016-03-16 11:36:35 +0100

    comment

--- Project1/Project1/Board.cs
+++ Project1/Project1/Board.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/*
+Author: Repsack
+Designed to represent the board and its knowledge about the pieces
+*/
+
 namespace Project1
 {
     class Board
