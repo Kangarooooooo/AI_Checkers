d13e1063bdeb8d17e534e77351ca71ad9ad427a9
Repsack <kh170391@gmail.com>
2016-04-06 09:32:54 +0200

    Finished constructor for GameManager

--- Project1/Project1/GameManager.cs
+++ Project1/Project1/GameManager.cs
@@ -13,7 +13,25 @@ namespace Project1
 {
     public class GameManager
     {
+        Board board;
         public GameManager()
+        //Author: Kasper
+        {
+            board = new Board();
+            Console.WriteLine("Welcome to checkers. A game for the cleverestest of people!");
+            Console.WriteLine("Which color does the player have? 1 for white, 2 for black");
+            int color;
+            String s = Console.ReadLine();
+            if (Int32.TryParse(s, out color))
+                Console.WriteLine("Color chosen.");
+            else
+                Console.WriteLine("String \""+s+"\" could not be interpreted as a number! YouDonGoofed");
+            board.showBoard();
+            playLoop(color);
+        }
+
+        private playLoop(int color)
+        //Author: Kasper
         {
 
         }
