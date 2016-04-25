aaa9f2457a4cf1e0d0539364c583113c70c3d33a
Repsack <kh170391@gmail.com>
2016-04-25 12:06:01 +0200

    Created OnePlayer class

---
+++ Project1/Project1/OnePlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    class OnePlayer
+    {
+        Boolean cap;
+        Board board;
+        MoveGenerator mg;
+        Move latestMove;
+        LinkedList<Move> legalActions, legalMoves, legalCaptures;
+        int n, choice, testCount;
+
+        public OnePlayer()
+        {
+            playLoop();
+        }
+
+        void playLoop()
+        {
+        }
+}
