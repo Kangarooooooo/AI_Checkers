3ff9cda428c03dfffac00cb2e2ec9b1dc8e127e8
kangarooooooo <eirikonielsen@gmail.com>
2016-04-26 21:28:29 +0200

    asdf

--- Project1/Project1/OnePlayer.cs
+++ Project1/Project1/OnePlayer.cs
@@ -23,7 +23,6 @@ namespace Project1
         {
             board = new Board();
             mg = new MoveGenerator(board);
-            ai = new AI(mg);
             latestMove = new Move();
             legalCaptures = new LinkedList<Move>();
             int n, choice, testCount;
@@ -51,7 +50,7 @@ namespace Project1
                 {
                     break;
                 }
-                AIblack();
+                //AIblack();
                 if (checkWin() != 0)
                 {
                     break;
@@ -63,7 +62,7 @@ namespace Project1
         {
             while (Math.Abs(board.evaluate()) < 1000) //no one is a winrar yet
             {
-                AIred();
+                //AIred();
                 checkWin();
                 if (checkWin() != 0)
                 {
@@ -196,16 +195,6 @@ namespace Project1
                 }
             }
             while (cap);
-        }
-
-        void AIred()
-        {
-            ai.MaximizerStart();
-        }
-
-        void AIblack()
-        {
-            ai.MinimizerStart();
         }
     }
 }
